3a60075b06288c35257be91790af5ba45c2f3a57
28shettr <28shettr@elmbrookstudents.org>
2026-07-02 15:10:47 -0500

    Intake.java written

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+import static com.pedropathing.ivy.commands.Commands.infinite;
+import static com.pedropathing.ivy.commands.Commands.instant;
+import static dev.nextftc.units.Units.Inches;
+
+import com.pedropathing.ivy.Command;
+
+import dev.nextftc.hardware.actuators.NextMotor;
+
+public class Intake {
+    NextMotor i = new NextMotor("intakeMotor", Inches.of(1.0), 0.2);
+    private IntakeState intakeState;
+    public enum IntakeState {
+        FORWARD,
+        REVERSE,
+        OFF
+    }
+
+    private double power;
+
+    private final double forward = 1.0;
+    private final double reverse = -1.0;
+    private final double off = 0.0;
+
+    public Intake(){
+        intakeState = IntakeState.OFF;
+        power = off;
+    }
+
+    private void setState(IntakeState intakeState) {
+        this.intakeState = intakeState;
+    }
+
+    public Command setSpeed(IntakeState s) {
+        return instant(() -> this.setState(s));
+    }
+
+
+    private void cycle(){
+        if (intakeState == IntakeState.REVERSE) power = forward;
+        else if (intakeState == IntakeState.FORWARD)  power = reverse;
+        else power = forward;
+    }
+
+    public Command periodic() {
+        return infinite(() -> {
+            switch (intakeState) {
+                case FORWARD:
+                    i.setThrottle(forward);
+                    break;
+                case REVERSE:
+                    i.setThrottle(reverse);
+                    break;
+                case OFF:
+                    i.setThrottle(off);
+                    break;
+            }
+        });
+    }
+
+
+}
